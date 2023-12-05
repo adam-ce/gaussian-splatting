b3b9d93e6d880d8470dab527685f412d958dd79f
Adam Celarek <5292991+adam-ce@users.noreply.github.com>
2023-12-06 00:52:16 +0100

    we can crank up  the learning rate for opacity again (after it being activated with exp)

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -76,7 +76,7 @@ class OptimizationParams(ParamGroup):
         self.position_lr_delay_mult = 0.01
         self.position_lr_max_steps = 30_000
         self.feature_lr = 0.0025
-        self.opacity_lr = 0.00001
+        self.opacity_lr = 0.01
         self.scaling_lr = 0.001
         self.rotation_lr = 0.001
         self.percent_dense = 0.01
