dcb28c24946d553ae15af00b45944cb384e823cf
Adam Celarek <5292991+adam-ce@users.noreply.github.com>
2023-12-06 00:52:53 +0100

    this shouldn't matter, as densification is disabled atm

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -85,7 +85,7 @@ class OptimizationParams(ParamGroup):
         self.opacity_reset_interval = 3000
         self.densify_from_iter = 500
         self.densify_until_iter = 15_000
-        self.densify_grad_threshold = 0.0002
+        self.densify_grad_threshold = 0.00002
         super().__init__(parser, "Optimization Parameters")
 
 def get_combined_args(parser : ArgumentParser):
