7e3c5e293621ea5cd0986900925695427089d1c4
Adam Celarek <5292991+adam-ce@users.noreply.github.com>
2024-06-01 03:38:32 +0200

    this allows arbitrary algorithm names in full eval with splatting (dirty hack)

--- gaussian_renderer/__init__.py
+++ gaussian_renderer/__init__.py
@@ -40,7 +40,7 @@ def render(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torch.Tensor, 
     scales = None
     rotations = None
     cov3D_precomp = None
-    if pipe.compute_cov3D_python and pipe.renderer == "inria_splatter":
+    if pipe.compute_cov3D_python and pipe.renderer != "vol_marcher":
         cov3D_precomp = pc.get_covariance(scaling_modifier)
     else:
         scales = pc.get_scaling
@@ -50,8 +50,8 @@ def render(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torch.Tensor, 
     # from SHs in Python, do it. If not, then SH -> RGB conversion will be done by rasterizer.
     shs = None
     colors_precomp = None
-    if override_color is None or pipe.renderer != "inria_splatter":
-        if pipe.convert_SHs_python and pipe.renderer == "inria_splatter":
+    if override_color is None or pipe.renderer == "vol_marcher":
+        if pipe.convert_SHs_python and pipe.renderer != "vol_marcher":
             shs_view = pc.get_features.transpose(1, 2).view(-1, 3, (pc.max_sh_degree+1)**2)
             dir_pp = (pc.get_xyz - viewpoint_camera.camera_center.repeat(pc.get_features.shape[0], 1))
             dir_pp_normalized = dir_pp/dir_pp.norm(dim=1, keepdim=True)
