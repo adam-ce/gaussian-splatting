eff4862b8d8832942f5c24d72187ba77d7e5dd97
Adam Celarek <5292991+adam-ce@users.noreply.github.com>
2024-06-21 13:29:08 +0200

    code style

--- full_eval.py
+++ full_eval.py
@@ -74,7 +74,8 @@ if not args.skip_training or not args.skip_rendering:
     args = parser.parse_args()
 
 if not args.skip_training:
-    common_args = " --quiet --eval --test_iterations -1  --save_iterations 5000 10000 15000 20000 30000 --iterations 30000 --densify_from_iter 10000000 --position_lr_init 0.00032 --feature_lr 0.0025 --scaling_lr 0.005 --rotation_lr 0.000125"
+    learning_rates = "--position_lr_init 0.00032 --feature_lr 0.0025 --scaling_lr 0.005 --rotation_lr 0.000125"
+    common_args = f" --quiet --eval --test_iterations -1  --save_iterations 5000 10000 15000 20000 30000 --iterations 30000 --densify_from_iter 10000000 {learning_rates}"
     for n_gaussians in n_gaussians_list:
         for algorithm, opacity_learning_rate, formulation in algorithms:
                 config_args = f" --renderer={algorithm} --opacity_lr {opacity_learning_rate} --formulation={formulation} --n_init_gaussians_for_synthetic {n_gaussians}"
